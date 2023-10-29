e1e8966654198e9532cbb5c155b60b8a5362e874
Robert Freigang <robert.freigang@projektmotor.de>
2023-10-29 23:42:48 +0100

    Perform composer update

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];

7f5b1a0b162e82f6b61dd93a3b3869bdd2a829ee
Robert Freigang <robert.freigang@projektmotor.de>
2023-10-31 13:16:11 +0100

    Use better wording for console logged debug text

--- assets/controllers/custom-autocomplete_controller.js
+++ assets/controllers/custom-autocomplete_controller.js
@@ -2,27 +2,26 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     initialize() {
-        console.log('init');
-        console.error('init');
+        console.log('initialize');
         this._onPreConnect = this._onPreConnect.bind(this);
         this._onConnect = this._onConnect.bind(this);
     }
 
     connect() {
-        console.log('init');
+        console.log('connect');
         this.element.addEventListener('autocomplete:pre-connect', this._onPreConnect);
         this.element.addEventListener('autocomplete:connect', this._onConnect);
     }
 
     disconnect() {
-        console.log('init');
+        console.log('disconnect');
         // You should always remove listeners when the controller is disconnected to avoid side-effects
         this.element.removeEventListener('autocomplete:pre-connect', this._onConnect);
         this.element.removeEventListener('autocomplete:connect', this._onPreConnect);
     }
 
     _onPreConnect(event) {
-        console.log('init');
+        console.log('_onPreConnect');
         // TomSelect has not been initialized - options can be changed
         console.log(event.detail.options); // Options that will be used to initialize TomSelect
         // event.detail.options.onChange = (value) => {
@@ -31,7 +30,7 @@ export default class extends Controller {
     }
 
     _onConnect(event) {
-        console.log('init');
+        console.log('_onConnect');
         // TomSelect has just been intialized and you can access details from the event
         console.log(event.detail.tomSelect); // TomSelect instance
         console.log(event.detail.options); // Options used to initialize TomSelect
